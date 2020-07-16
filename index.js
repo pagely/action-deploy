26af3900fda50622014778fef2f14b74517cc9d6
Joshua Eichorn <joshua.eichorn@pagely.com>
2020-07-16 19:31:51 +0000

    make lint nicer, and put everything in a wrapper func so I can use await

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const octokit = require('@octokit/core');
-const http = require('@actions/http-client');
+(async () => {
+    const core = require('@actions/core');
+    const {Octokit} = require('@octokit/rest');
+    const octokit = new Octokit();
+    //const http = require('@actions/http-client');
 
-try {
+    try {
 
-    const deployUrl = core.getInput('deploy-url');
-    const artifactName = core.getInput('artifact-name');
+        //const deployUrl = core.getInput('deploy-url');
+        const artifactName = core.getInput('artifact-name');
+        console.log(`artifactName: ${artifactName}`)
 
-    // debug stuff
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
-    // remove this stuff
+        const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+        const run_id = process.env.GITHUB_RUN_ID;
 
-    const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
-    const run_id = process.env.GITHUB_RUN_ID
-
-    const {data: artifacts} = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/artifacts, {
-        owner: owner,
-        repo: repo,
-        run_id: run_id,
-    })
-    const payload = JSON.stringify(artifacts, undefined, 2);
-    console.log(`run artifacts: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
-}
+        const {data: artifacts} = await octokit.listWorkflowRunArtifacts({
+            owner: owner,
+            repo: repo,
+            run_id: run_id,
+        });
+        const debug = JSON.stringify(artifacts, undefined, 2);
+        console.log(`run artifacts: ${debug}`);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+})();
